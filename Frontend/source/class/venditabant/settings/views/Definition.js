692a15e103116569c79d4a9f73ebb2169b18bcf8
janeskil1525 <janeskil1525@gmail.com>
2021-10-04 17:11:30 +0200

    Product group

--- Frontend/source/class/venditabant/settings/views/Definition.js
+++ Frontend/source/class/venditabant/settings/views/Definition.js
@@ -28,6 +28,9 @@ qx.Class.define ( "venditabant.settings.views.Definition",
                 let box1 = new venditabant.settings.views.VatBox().getView();
                 page1.add ( box1, { top: 0, left: 10 } );
 
+                let box2 = new venditabant.settings.views.ProdGrpBox().getView();
+                page1.add ( box2, { top: 0, left: 200 } );
+
                 tabView.add(page1);
 
                 var page2 = new qx.ui.tabview.Page("Stock");
--- Frontend/source/class/venditabant/settings/views/ProdGrpBox.js
+++ Frontend/source/class/venditabant/settings/views/ProdGrpBox.js
@@ -1,4 +1,4 @@
-qx.Class.define ( "venditabant.settings.views.VatBox",
+qx.Class.define ( "venditabant.settings.views.ProdGrpBox",
     {
             extend: venditabant.application.base.views.Base,
             include: [qx.locale.MTranslation],
@@ -12,44 +12,44 @@ qx.Class.define ( "venditabant.settings.views.VatBox",
             members: {
                     // Public functions ...
                     getView: function () {
-                        let box1 = new qx.ui.groupbox.GroupBox(this.tr("VAT"), null);
+                        let box1 = new qx.ui.groupbox.GroupBox(this.tr("Product groups"), null);
                         box1.setLayout(new qx.ui.layout.VBox());
                         let container = new qx.ui.container.Composite(new qx.ui.layout.HBox(6));
 
-                        let lbl = this._createLbl(this.tr( "Percentage" ),70);
-                        let vat_txt = this._createTxt(
-                            this.tr( "VAT" ),60,true,this.tr("VAT is required")
+                        let lbl = this._createLbl(this.tr( "Product group" ),90);
+                        let prdgrp_txt = this._createTxt(
+                            this.tr( "Product group" ),100,true,this.tr("Product group is required")
                         );
-                        this._vat_txt = vat_txt;
-                        vat_txt.addListener("input",function(e){
+                        this._prdgrp_txt = prdgrp_txt;
+                        prdgrp_txt.addListener("input",function(e){
                             let value = e.getData();
                         },this);
                         container.add(lbl,{flex:1});
-                        container.add(vat_txt);
+                        container.add(prdgrp_txt);
                         box1.add(container);
 
-                        let vat_desc = this._createTxt(
-                            this.tr( "Description" ),120,true,this.tr("VAT is required")
+                        let prdgrp_desc = this._createTxt(
+                            this.tr( "Description" ),120,true,this.tr("Product group is required")
                         );
-                        this._vat_desc = vat_desc;
-                        box1.add(vat_desc);
-                        let save_VAT = this._createBtn ( this.tr ( "Save" ), "rgba(239,170,255,0.44)", 120, function ( ) {
-                            this.saveVAT ( );
+                        this._prdgrp_desc = prdgrp_desc;
+                        box1.add(prdgrp_desc);
+                        let save_PRDGRP = this._createBtn ( this.tr ( "Save" ), "rgba(239,170,255,0.44)", 120, function ( ) {
+                            this.save_PRDGRP ( );
                         }, this );
-                        box1.add(save_VAT);
+                        box1.add(save_PRDGRP);
 
                         var oneList = new qx.ui.form.List();
                         oneList.addListener("changeSelection",function(e) {
                             let selection = e.getData()[0].getLabel();
-                            let vat_txt = selection.substring(0,selection.indexOf(' '));
-                            let vat_desc = selection.substring(selection.indexOf(' '));
+                            let prdgrp_txt = selection.substring(0,selection.indexOf(' '));
+                            let prdgrp_desc = selection.substring(selection.indexOf(' '));
 
-                            this._vat_txt.setValue(vat_txt);
-                            this._vat_desc.setValue(vat_desc);
+                            this._prdgrp_txt.setValue(prdgrp_txt);
+                            this._prdgrp_desc.setValue(prdgrp_desc);
 
                         },this)
 
-                        oneList.set({ height: 100, width: 150, selectionMode : "one" });
+                        oneList.set({ height: 100, width: 200, selectionMode : "one" });
                         let get = new venditabant.settings.models.Settings();
                         get.loadList(function(response) {
                             var item;
@@ -58,7 +58,7 @@ qx.Class.define ( "venditabant.settings.views.VatBox",
                                 item = new qx.ui.form.ListItem(row, null);
                                 oneList.add(item);
                             }
-                        },this,'VAT');
+                        },this,'PRODUCTGROUPS');
 
                         box1.add(oneList);
 

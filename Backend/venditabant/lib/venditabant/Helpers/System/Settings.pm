8a21032e746a6f3296e4853aad41ed708b6e50b2
janeskil1525 <janeskil1525@gmail.com>
2021-11-03 08:07:01 +0100

    System settings 3

--- Backend/venditabant/lib/venditabant/Helpers/System/Settings.pm
+++ Backend/venditabant/lib/venditabant/Helpers/System/Settings.pm
@@ -9,16 +9,24 @@ use Data::Dumper;
 has 'pg';
 
 async sub upsert ($self, $companies_pkey, $users_pkey, $data) {
+    my $err;
+    eval {
+        $data->{value} = await venditabant::Helpers::Jwt->new()->encode_jwt_p($data->{value});
 
-    $data->{value} = await venditabant::Helpers::Jwt->new()->encode_jwt_p($data->{value});
+        my $system_settings_pkey = await venditabant::Model::System::Settings->new(
+            db => $self->pg->db
+        )->upsert(
+            $companies_pkey, $users_pkey, $data
+        );
+    };
+    $err = $@ if $@;
+    $self->capture_message (
+        $self->pg, '',
+        'venditabant::Helpers::Stockitems', 'upsert', $@
+    ) if $err;
 
-    my $system_settings_pkey = await venditabant::Model::System::Settings->new(
-        db => $self->pg->db
-    )->upsert(
-        $companies_pkey, $users_pkey, $data
-    );
-
-    return $system_settings_pkey;
+    return 'success' unless $err;
+    return $err;
 }
 
 async sub load_system_setting($self, $companies_pkey, $users_pkey, $setting) {

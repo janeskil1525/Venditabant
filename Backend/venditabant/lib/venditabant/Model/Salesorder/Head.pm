ad941e17eac0192127a9fdf7e2b073ca468f0ae4
janeskil1525 <janeskil1525@gmail.com>
2021-12-27 11:30:22 +0100

    Workflow integration Code cleanup

--- Backend/venditabant/lib/venditabant/Model/Salesorder/Head.pm
+++ Backend/venditabant/lib/venditabant/Model/Salesorder/Head.pm
@@ -40,36 +40,6 @@ async sub load_salesorder_list ($self, $companies_pkey, $users_pkey, $open) {
     return $hash;
 }
 
-async sub upsert ($self, $companies_pkey, $users_pkey, $data) {
-
-    my $salesorders_stmt = qq{
-        INSERT INTO salesorders (
-                insby, modby, companies_fkey, users_fkey, customers_fkey, orderno
-            ) VALUES (
-                    (SELECT userid FROM users WHERE users_pkey = ?),
-                    (SELECT userid FROM users WHERE users_pkey = ?),?,?,?,?)
-            ON CONFLICT (orderno)
-            DO UPDATE SET modby = (SELECT userid FROM users WHERE users_pkey = ?),
-                        moddatetime = now()
-            RETURNING salesorders_pkey
-    };
-
-    my $salesorders_pkey= $self->db->query(
-        $salesorders_stmt,
-        (
-            $users_pkey,
-            $users_pkey,
-            $companies_pkey,
-            $users_pkey,
-            $data->{customers_fkey},
-            $data->{orderno},
-            $users_pkey,
-        )
-    )->hash->{salesorders_pkey};
-
-    return $salesorders_pkey;
-}
-
 async sub get_open_so ($self, $companies_pkey, $customer_fkey) {
 
     my $soopen_stmt = qq {
@@ -110,31 +80,6 @@ async sub get_open_so_pkey ($self, $companies_pkey, $customer) {
     $hash = $result->hash if $result and $result->rows;
     if (defined $hash) {
         return $hash->{orderno};
-    }
-    return 0;
-}
-
-async sub close ($self, $companies_pkey, $users_pkey, $customer_fkey) {
-
-    my $soclose_stmt = qq {
-        UPDATE salesorders SET open = false,
-            modby = (SELECT userid FROM users WHERE users_pkey = ?),
-            moddatetime = now()
-            WHERE open = true AND
-            companies_fkey = ?
-            AND customers_fkey = ?
-        RETURNING salesorders_pkey
-    };
-
-    my $result = $self->db->query(
-        $soclose_stmt,
-        ($users_pkey, $companies_pkey, $customer_fkey)
-    );
-
-    my $hash;
-    $hash = $result->hash if $result and $result->rows;
-    if (defined $hash) {
-        return $hash->{salesorders_pkey};
     }
     return 0;
 }
--- Backend/venditabant/lib/venditabant/Model/Salesorder/Item.pm
+++ Backend/venditabant/lib/venditabant/Model/Salesorder/Item.pm
@@ -25,67 +25,6 @@ async sub load_items_list ($self, $companies_pkey, $users_pkey, $salesorders_pke
     return $hash;
 }
 
-async sub delete_item ($self, $companies_pkey, $salesorders_pkey, $data) {
-
-    my $salesorder_item_stmt = qq{
-        DELETE FROM salesorder_items WHERE salesorders_fkey = ?
-            AND stockitems_fkey = ?
-    };
-
-    $self->db->query(
-        $salesorder_item_stmt,
-            ($salesorders_pkey, $data->{stockitems_fkey})
-    );
-}
-
-async sub upsert ($self, $companies_pkey, $salesorders_pkey, $users_pkey, $data) {
-
-    my $salesorder_item_stmt = qq{
-        INSERT INTO salesorder_items (
-                insby, modby, salesorders_fkey, stockitem, description,
-                quantity, price, customer_addresses_fkey, vat, discount, unit, account
-            ) VALUES (
-                    (SELECT userid FROM users WHERE users_pkey = ?),
-                    (SELECT userid FROM users WHERE users_pkey = ?),?,?,?,?,?,?,?,?,?,?)
-            ON CONFLICT (salesorders_fkey, stockitem)
-            DO UPDATE SET modby = (SELECT userid FROM users WHERE users_pkey = ?),
-                        moddatetime = now(),
-                        quantity = ?, price = ?, customer_addresses_fkey = ?,
-                        description = ?, vat = ?, discount = ?, deliverydate = now(),
-                        unit = ?, account = ?
-            RETURNING salesorder_items_pkey
-    };
-
-    my $salesorder_items_pkey = $self->db->query(
-        $salesorder_item_stmt,
-        (
-            $users_pkey,
-            $users_pkey,
-            $salesorders_pkey,
-            $data->{stockitem},
-            $data->{description},
-            $data->{quantity},
-            $data->{price},
-            $data->{customer_addresses_pkey},
-            $data->{vat},
-            $data->{discount},
-            $data->{unit},
-            $data->{account},
-            $users_pkey,
-            $data->{quantity},
-            $data->{price},
-            $data->{customer_addresses_pkey},
-            $data->{description},
-            $data->{vat},
-            $data->{discount},
-            $data->{unit},
-            $data->{account},
-        )
-    )->hash->{salesorder_items_pkey};
-
-    return $salesorder_items_pkey;
-}
-
 async sub get_orderitems ($self, $salesorders_pkey) {
 
     my $result = $self->db->select(

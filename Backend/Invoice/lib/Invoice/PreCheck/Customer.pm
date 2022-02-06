0c0128e4016f330a83e7d18239116f5a5ef6aaa7
janeskil1525 <janeskil1525@gmail.com>
2022-02-06 14:19:31 +0100

    Tested

---
+++ Backend/Invoice/lib/Invoice/PreCheck/Customer.pm
@@ -0,0 +1,30 @@
+package Invoice::PreCheck::Customer;
+use Mojo::Base -base, -signatures, -async_await;
+
+use Data::Dumper;
+
+has 'pg';
+
+
+async sub find_customer($self, $invoice_fkey, $data) {
+
+    my $stmt = qq {
+        SELECT customers_pkey, a.name, a.companies_fkey, a.languages_fkey FROM customers a JOIN invoice b
+        ON customers_pkey = customers_fkey AND invoice_pkey = ?
+    };
+
+    my $result = $self->pg->db->query($stmt,($invoice_fkey));
+
+    my $hash;
+    $hash = $result->hash if defined $result and $result->rows() > 0;
+
+    if(defined $hash) {
+        $data->{customer}->{name} = $hash->{name};
+        $data->{customer}->{companies_fkey} = $hash->{companies_fkey};
+        $data->{customer}->{languages_fkey} = $hash->{languages_fkey};
+    }
+
+    return $data;
+}
+
+1;

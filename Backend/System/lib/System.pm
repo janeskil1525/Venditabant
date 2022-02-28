4ddccd47c020a4af1c4915db27cc142e4d185106
janeskil1525 <janeskil1525@gmail.com>
2022-02-28 16:14:44 +0100

    System

---
+++ Backend/System/lib/System.pm
@@ -0,0 +1,6 @@
+package System;
+use strict;
+use warnings FATAL => 'all';
+
+our $VERSION = '0.02';
+1;
--- Backend/System/lib/System/Model/Settings.pm
+++ Backend/System/lib/System/Model/Settings.pm
@@ -1,11 +1,25 @@
-package venditabant::Model::System::Settings;
-use Mojo::Base 'venditabant::Helpers::Sentinel::Sentinelsender', -signatures, -async_await;
+package System::Model::Settings;
+use Mojo::Base -base, -signatures, -async_await;
 
 use Data::Dumper;
 
 has 'db';
 
-async sub load_setting($self, $setting) {
+sub load_setting($self, $setting) {
+
+    my $result = $self->db->select(
+        'system_settings', ['*'],
+        {
+            setting => $setting
+        }
+    );
+
+    my $hash;
+    $hash = $result->hash if $result and $result->rows > 0;
+    return $hash;
+}
+
+async sub load_setting_p($self, $setting) {
 
     my $result = $self->db->select(
         'system_settings', ['*'],

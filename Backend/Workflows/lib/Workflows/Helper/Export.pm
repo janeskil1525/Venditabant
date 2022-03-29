dc8cd8b728b184e61b08eccb5705f7b6f380248f
janeskil1525 <janeskil1525@gmail.com>
2022-03-29 18:09:41 +0200

    Export workflow

--- Backend/Workflows/lib/Workflows/Helper/Export.pm
+++ Backend/Workflows/lib/Workflows/Helper/Export.pm
@@ -22,26 +22,26 @@ async sub export($self) {
         $stmt .= await $self->generate_wfl_upsert($workflow->{workflow});
         my $items = await $wf_items->load_workflow_items($workflow->{workflows_pkey});
         foreach my $item (@{$items}) {
-            $stmt .= $self->generate_wfl_upsert_item($item);
+            $stmt .= await $self->generate_wfl_upsert_item($workflow->{workflow}, $item);
         }
     }
 
-    my $path = Mojo::File->new('workflow.sql')->spurt($stmt);
+    my $path = Mojo::File->new('/home/jan/Publikt/workflow.sql')->spurt($stmt);
 
 }
 
 async sub generate_wfl_upsert_item($self, $workflow, $item) {
 
     my $workflow_type = $item->{workflow_type};
-    my $workflow = $item->{workflow};
+    my $workflow_data = $item->{workflow};
     return qq{
         INSERT INTO workflow_items (workflows_fkey, workflow_type, workflow)
         VALUES (
             (SELECT workflows_pkey FROM workflows WHERE workflow = '$workflow'),
-            '$workflow_type', '$workflow'
+            '$workflow_type', '$workflow_data'
         )
         ON CONFLICT( workflows_fkey, workflow_type)
-        DO UPDATE SET moddatetime = now(), workflow = '$item->workflow'
+        DO UPDATE SET moddatetime = now(), workflow = '$workflow_data' ;
     };
 }
 
